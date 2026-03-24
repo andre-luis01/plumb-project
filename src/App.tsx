6d1e7c1441126d5d28b865a29aa75e4e551884e4
andre-luis01 <andre.luiz0142@gmail.com>
2026-03-24 11:25:48 -0300

    commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+  
+      
+        <div>
+          <h1>Bem-vindo a Plumb Quantium Solutions!</h1>
+          <h3>Vamos entender como você está se sentindo no trabalho</h3>
+
+        </div>
+      
+     
+  )
+}
+
+export default App
